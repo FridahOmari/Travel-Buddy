1197fd797c2c6c8f30a54a9941d04dd6b3f99b24
fridah <nginaomari@gmail.com>
2022-06-24 21:50:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,66 +1,49 @@
 window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed')
     getParks()
-    setTimeout(() => {
-        renderParks()
-    }, 
-    1000);
-})
-
-
-// Fetch
-let parks = []
-
-function getParks() {
+    addEventListeners()
+  })
+  // Fetch
+  let parks
+  function getParks() {
     const url = 'https://developer.nps.gov/api/v1/parks?api_key=wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk&limit=10'
     fetch(url)
-        .then(res => {
-            return res.json()
-        })
-        .then(p => {
-            parks.push(p.data)
-            console.log(parks)
-
-        })
-     
-
-}
-
-
-
-// Render Parks Function
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
-function renderParks() {
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        parks = data
+        renderParks()
+      })
+  }
+  // Render Parks Function
+  var LIKES = 0
+  function renderParks() {
+    console.log("hello")
     const parkDiv = document.getElementById('parks')
     parkDiv.innerHTML = ' '
-
-    for (let i = 0; i < parks.length; i++) {
-
-        const ul = document.createElement('ul')
-        const img = document.createElement('img')
-        const descrip = document.createElement('p')
-        const directions = document.createElement('a')
-        const liker = document.createElement('button')
-        liker.textContent = EMPTY_HEART
-        directions.innerText = 'Get Directions'
-        directions.href = parks[i].directionsUrl
-        directions.target = '_blank'
-        ul.innerText = parks[i].fullName
-        descrip.innerText = parks[i].description
-        parkDiv.append(ul, descrip, directions, liker)
-        liker.addEventListener('click', likePark)
+    let filteredParks = parks.data
+    console.log(filteredParks)
+    for (let i = 0; i < filteredParks.length; i++) {
+      const ul = document.createElement('ul')
+      const img = document.createElement('img')
+      const descrip = document.createElement('p')
+      const directions = document.createElement('a')
+      const liker = document.createElement('button')
+      liker.textContent = LIKES
+      directions.innerText = 'Get Directions'
+      directions.href = filteredParks[i].directionsUrl
+      directions.target = '_blank'
+      img.src = filteredParks[i].images[0].url
+      ul.innerText = filteredParks[i].fullName
+      descrip.innerText = filteredParks[i].description
+      parkDiv.append(img, ul, descrip, directions, liker)
+      liker.addEventListener('click', likePark)
     }
-}
-
-
-const likePark = (e) => {
+  }
+  const likePark = (e) => {
     const liker = e.target
-    const like = liker.textContent
-    if (like === EMPTY_HEART) {
-        liker.textContent = FULL_HEART
-    } else {
-        liker.textContent = EMPTY_HEART
-    }
-}
-
+    LIKES++
+    console.log(LIKES)
+    liker.textContent = LIKES
+  }
