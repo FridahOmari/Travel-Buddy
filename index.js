c6731a53eafa90d93aa6cc9df59b069024f59cc2
fridah <nginaomari@gmail.com>
2022-06-29 15:19:32 +0300

    fetched parks data from API

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ window.addEventListener('DOMContentLoaded', () => {
   getParks()
   addEventListeners()
 })
+
+// Fetch
+let parks
+function getParks() {
+  const url = 'https://developer.nps.gov/api/v1/parks?api_key=wSlcKl1LSOpHXf164FTShbQZC4cmu1VWTJiipnFk&limit=10'
+  fetch(url)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      parks = data
+      renderParks()
+    })
+}
